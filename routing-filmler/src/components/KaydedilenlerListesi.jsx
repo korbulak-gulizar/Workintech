8eea18830649fbd95d2f0216e43a3d1b80d08c9b
Gulizar Korbulak <korbulakgulizar@gmail.com>
2026-01-06 08:47:18 +0100

    Router ve flimler

---
+++ routing-filmler/src/components/KaydedilenlerListesi.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function KaydedilenlerListesi(props) {
+  return (
+    <div className="saved-list">
+      <h3>Kaydedilen Filmler:</h3>
+      {props.list.map((movie, index) => (
+        <span key={index} className="saved-movie">
+          {movie.title}
+        </span>
+      ))}
+
+      <Link to="/" className="home-button">
+        Anasayfa
+      </Link>
+    </div>
+  );
+}
